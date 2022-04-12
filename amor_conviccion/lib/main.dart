281704809ac4f57fd2451206b60b3a624aea9de8
Juan Renteria <juanrenteria55@gmail.com>
2022-04-12 13:35:28 -0700

    Initial Firebase
    Configuracion inicial de firebase

--- amor_conviccion/lib/main.dart
+++ amor_conviccion/lib/main.dart
@@ -1,12 +1,17 @@
-import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() {
-  runApp(const MyApp());
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
+  MyApp({Key? key}) : super(key: key);
+  final Future<FirebaseApp> _fbapp = Firebase.initializeApp();
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -24,7 +29,21 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: FutureBuilder(
+        future: _fbapp,
+        builder: (context, snapshot){
+          if(snapshot.hasError){
+            print('You have an error! ${snapshot.error.toString()}');
+            return Text('Something went wrong!');
+          }else if (snapshot.hasData){
+            return const MyHomePage(title: 'Flutter Demo Home Page');
+          }else{
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      )
     );
   }
 }
@@ -51,6 +70,8 @@ class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
   void _incrementCounter() {
+    DatabaseReference _test = FirebaseDatabase.instance.ref().child("test");
+    _test.set("Hello World ${Random().nextInt(100)}");
     setState(() {
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
