c7bde48d6dd6e286923851d34ff99744c3135e1d
DPRene <l18330420@hermosillo.tecnm.mx>
2022-04-21 20:03:10 -0700

    Prueba

--- amor_conviccion/lib/services/userData.dart
+++ amor_conviccion/lib/services/userData.dart
@@ -1,37 +1,61 @@
 import 'package:firebase_auth/firebase_auth.dart';
+<<<<<<< HEAD
+=======
+import 'package:flutter/services.dart';
+>>>>>>> parent of 7802e83 (Email Firebase)
 
-class EmailSignInProvider{
+class UserData {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   get user => _auth.currentUser;
 
   //SIGN UP METHOD
-  Future signUp({required String email, required String password, required String name}) async {
+  Future signUpEmail(String email, String password) async {
     try {
+<<<<<<< HEAD
       UserCredential credential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
         email: email,
         password: password,
+=======
+      UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+          email: email,
+          password: password
+>>>>>>> parent of 7802e83 (Email Firebase)
       );
-      User? updateUser = FirebaseAuth.instance.currentUser;
-      updateUser!.updateDisplayName(name);
-      return null;
     } on FirebaseAuthException catch (e) {
-      return e.message;
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        print('The account already exists for that email.');
+      }else{
+        print("el error es $e");
+      }
     }
   }
 
   //SIGN IN METHOD
   Future signIn({required String email, required String password}) async {
     try {
-      await _auth.signInWithEmailAndPassword(email: email, password: password);
-      return null;
+      UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: email,
+          password: password
+      );
     } on FirebaseAuthException catch (e) {
-      return e.message;
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for that user.');
+      }
     }
   }
+
 
   //SIGN OUT METHOD
   Future<void>signOut() async {
     await _auth.signOut();
+<<<<<<< HEAD
     FirebaseAuth.instance.signOut();
+=======
+>>>>>>> parent of 7802e83 (Email Firebase)
   }
 }
+
