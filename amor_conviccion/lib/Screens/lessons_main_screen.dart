67e62f8da302b4edec758fe74fc9d9e681e44398
DPRene <l18330420@hermosillo.tecnm.mx>
2022-04-26 10:24:04 -0700

    Segundo avance Lecciones

--- amor_conviccion/lib/Screens/lessons_main_screen.dart
+++ amor_conviccion/lib/Screens/lessons_main_screen.dart
@@ -12,56 +12,189 @@ class MainLessons extends StatefulWidget {
 class _MainLessons extends State<MainLessons> {
   Widget main(Size size) {
     return Scaffold(
-        body: Column(
-      children: [
-        SizedBox(
-          height: size.height * 0.05,
-        ),
-        SizedBox(
-          //color: Color.fromARGB(255, 255, 134, 134),
-          width: double.infinity,
-          child: Row(children: [
-            SizedBox(
-              //color: Colors.red,
-              width: size.width * 0.5,
-              height: size.height * 0.2,
-              child: Image.asset(
-                'assets/logos/pentagono.png',
-              ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: size.height * 0.05,
+          ),
+          SizedBox(
+            width: double.infinity,
+            child: Row(
+              children: [
+                SizedBox(
+                  width: size.width * 0.5,
+                  height: size.height * 0.2,
+                  child: Image.asset(
+                    'assets/logos/pentagono.png',
+                  ),
+                ),
+                SizedBox(
+                  width: size.width * 0.5,
+                  height: size.height * 0.2,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'El Pentagono',
+                        style: TextStyle(
+                            fontSize: size.height * 0.035,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        'Curso',
+                        style: TextStyle(
+                          fontSize: size.height * 0.035,
+                          color: const Color.fromARGB(181, 0, 0, 0),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
             ),
-            SizedBox(
-              //color: Colors.orange,
-              width: size.width * 0.5,
-              height: size.height * 0.2,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 40),
+            width: size.width * 0.8,
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 255, 255, 255),
+              borderRadius: const BorderRadius.all(
+                Radius.circular(15),
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: const Offset(0, 3), // changes position of shadow
+                ),
+              ],
+            ),
+            child: TextButton(
+              onPressed: null,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  Text(
-                    'El Pentagono',
-                    style: TextStyle(
-                        fontSize: size.height * 0.035,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  Text(
-                    'Curso',
-                    style: TextStyle(
-                      fontSize: size.height * 0.035,
-                      color: Color.fromARGB(181, 0, 0, 0),
-                    ),
-                  ),
+                  Icon(Icons.person, size: size.height * 0.075),
+                  Column(
+                    children: [
+                      Text(
+                        'Drogodependencia',
+                        style: TextStyle(
+                          fontSize: size.height * 0.025,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                      Text(
+                        'Bloque 1',
+                        style: TextStyle(
+                          fontSize: size.height * 0.0225,
+                          color: const Color.fromARGB(181, 0, 0, 0),
+                        ),
+                      ),
+                    ],
+                  )
                 ],
               ),
-            )
-          ]),
-        ),
-        Container(
-            child: ElevatedButton.icon(
-                onPressed: null,
-                icon: const Icon(Icons.search),
-                label: const Text('Pene'))),
-      ],
-    ));
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 60),
+            width: size.width * 0.8,
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 255, 255, 255),
+              borderRadius: const BorderRadius.all(
+                Radius.circular(15),
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: const Offset(0, 3), // changes position of shadow
+                ),
+              ],
+            ),
+            child: TextButton(
+              onPressed: null,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Icon(Icons.person, size: size.height * 0.075),
+                  Column(
+                    children: [
+                      Text(
+                        'Drogodependencia',
+                        style: TextStyle(
+                          fontSize: size.height * 0.025,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                      Text(
+                        'Bloque 2',
+                        style: TextStyle(
+                          fontSize: size.height * 0.0225,
+                          color: const Color.fromARGB(181, 0, 0, 0),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 60),
+            width: size.width * 0.8,
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 255, 255, 255),
+              borderRadius: const BorderRadius.all(
+                Radius.circular(15),
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: const Offset(0, 3), // changes position of shadow
+                ),
+              ],
+            ),
+            child: TextButton(
+              onPressed: null,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Icon(Icons.person, size: size.height * 0.075),
+                  Column(
+                    children: [
+                      Text(
+                        'Drogodependencia',
+                        style: TextStyle(
+                          fontSize: size.height * 0.025,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                      Text(
+                        'Bloque 3',
+                        style: TextStyle(
+                          fontSize: size.height * 0.0225,
+                          color: const Color.fromARGB(181, 0, 0, 0),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
   @override
