e8abf003f55ebc8ccbfdd4537691215401973c49
DPRene <l18330420@hermosillo.tecnm.mx>
2022-05-09 20:48:44 -0700

    Creacion pantalla lecturas

---
+++ amor_conviccion/lib/Screens/Lessons/lecture_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LectureScreen extends StatefulWidget {
+  const LectureScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LectureScreen> createState() => _LectureScreenState();
+}
+
+class _LectureScreenState extends State<LectureScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
