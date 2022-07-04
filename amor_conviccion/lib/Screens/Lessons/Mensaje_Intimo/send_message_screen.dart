ba2e9f210875cfb73489e0a03091fc4f12c8edfb
Juan Renteria <juanrenteria55@gmail.com>
2022-07-04 14:59:32 -0700

    estructura codigo mensajes

--- amor_conviccion/lib/Screens/Lessons/Mensaje_Intimo/send_message_screen.dart
+++ amor_conviccion/lib/Screens/Lessons/Mensaje_Intimo/send_message_screen.dart
@@ -1,14 +1,11 @@
 import 'package:amor_conviccion/utils/mensajes_intimidad.dart';
+import 'package:amor_conviccion/widgets/intimidad/mensajes.dart';
 import 'package:flutter/material.dart';
-import 'package:share_plus/share_plus.dart';
 
 class SendMessage extends StatelessWidget {
   SendMessage(this.opc, {Key? key}) : super(key: key);
 
   final int opc;
-
-  List<TextEditingController> txtcontroller =
-      List.generate(3, (i) => TextEditingController());
 
   MapMensajes mensajes = MapMensajes();
   late Map textos;
@@ -16,7 +13,6 @@ class SendMessage extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
 
-    int cont = 0;
     switch (opc) {
       case 1:
         textos = mensajes.padres_hijos;
@@ -48,68 +44,7 @@ class SendMessage extends StatelessWidget {
               itemCount: 3,
               itemBuilder: (context, index) {
                 pagina = textos[index];
-                return Center(
-                  child: Padding(
-                    padding: const EdgeInsets.all(10),
-                    child: SizedBox(
-                      child: Column(
-                        children: [
-                          Container(
-                            decoration: BoxDecoration(
-                              boxShadow: const [
-                                BoxShadow(
-                                  blurRadius: 3,
-                                ),
-                              ],
-                              borderRadius: BorderRadius.circular(5.0),
-                            ),
-                            child: TextFormField(
-                              readOnly: true,
-                              controller: txtcontroller[index]
-                                ..text = pagina![0],
-                              decoration: InputDecoration(
-                                fillColor: Colors.white,
-                                filled: true,
-                                contentPadding: EdgeInsets.fromLTRB(
-                                  size.width * 0.03,
-                                  size.height * 0.01,
-                                  size.width * 0.03,
-                                  size.height * 0.01,
-                                ),
-                                enabledBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(5.0),
-                                  borderSide: const BorderSide(
-                                    color: Colors.white,
-                                    width: 3.0,
-                                  ),
-                                ),
-                              ),
-                              maxLines: 5,
-                              style: const TextStyle(color: Colors.black),
-                            ),
-                          ),
-                          SizedBox(
-                            width: size.width * 0.3,
-                            child: ElevatedButton.icon(
-                              onPressed: () {
-                                Share.share(txtcontroller[index].text);
-                              },
-                              style: ButtonStyle(
-                                backgroundColor:
-                                    MaterialStateProperty.all(Colors.blue),
-                              ),
-                              icon: Icon(Icons.share),
-                              label: const Text(
-                                'Enviar',
-                                style: TextStyle(fontWeight: FontWeight.bold),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                );
+                return Mensajes(index, pagina);
               },
             ),
           ),
--- amor_conviccion/lib/widgets/intimidad/mensajes.dart
+++ amor_conviccion/lib/widgets/intimidad/mensajes.dart
@@ -1,7 +1,78 @@
 import 'package:flutter/material.dart';
+import 'package:share_plus/share_plus.dart';
 
-class Mensajes {
-  Widget mtextfield() {
-    return TextFormField();
+class Mensajes extends StatelessWidget {
+  Mensajes(this.index, this.mensajes, {Key? key}) : super(key: key);
+
+  final int index;
+  final List? mensajes;
+  List<TextEditingController> txtcontroller =
+      List.generate(3, (i) => TextEditingController());
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: SizedBox(
+          child: Column(
+            children: [
+              Container(
+                  decoration: BoxDecoration(
+                    boxShadow: const [
+                      BoxShadow(
+                        blurRadius: 3,
+                      ),
+                    ],
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                  child: RawScrollbar(
+                    thumbColor: const Color(0xFF42ADE2),
+                    thickness: 2,
+                    child: TextFormField(
+                      readOnly: true,
+                      controller: txtcontroller[index]..text = mensajes![0],
+                      decoration: InputDecoration(
+                        fillColor: Colors.white,
+                        filled: true,
+                        contentPadding: EdgeInsets.fromLTRB(
+                          size.width * 0.03,
+                          size.height * 0.01,
+                          size.width * 0.03,
+                          size.height * 0.01,
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5.0),
+                          borderSide: const BorderSide(
+                            color: Colors.white,
+                            width: 3.0,
+                          ),
+                        ),
+                      ),
+                      maxLines: 5,
+                      style: const TextStyle(color: Colors.black),
+                    ),
+                  )),
+              SizedBox(
+                width: size.width * 0.3,
+                child: ElevatedButton.icon(
+                  onPressed: () {
+                    Share.share(txtcontroller[index].text);
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.blue),
+                  ),
+                  icon: const Icon(Icons.share),
+                  label: const Text(
+                    'Enviar',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
